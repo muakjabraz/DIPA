08c1302066ebaeca37b4d3bd7a9883750fa385cc
Muhammad Akita Jabbar Razzaq <majabbarrazzaq@gmail.com>
2022-09-16 19:24:26 +0700

    first commit

---
+++ pages/about.php
@@ -0,0 +1,14 @@
+<h1>About</h1>
+<div class="row">
+    <div class="col-md-auto">
+        <p><img style="height:120px;width:100px;" src="asset/profil1.jpg" class="rounded" alt=""></p>
+    </div>
+    <div class="col">
+        <ul>
+            <li><b>Aplikasi Ini dibuat oleh :</b></li>
+            <li>Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Muhammad Akita Jabbar Razzaq</li>
+            <li>NIM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: 1931710142</li>
+            <li>Tanggal&nbsp;&nbsp;: 13 September 2022</li>
+        </ul>
+    </div>
+</div>
